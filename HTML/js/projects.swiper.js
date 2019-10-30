89494c85a302a62f70439ca66706290ba139ad27
bo-co <bo-co@ya.ru>
2019-10-30 19:27:34 +0300

    main.update

---
+++ HTML/js/projects.swiper.js
@@ -0,0 +1,30 @@
+var suppliers = null;
+
+$(document).ready(function() {
+ 	if (!suppliers && $('div.swiper-projects').length !== 0) {
+ 		setTimeout(function() {
+			suppliers = new Swiper('div.swiper-projects', {
+    			loop: true,
+ 				slidesPerView: 1,
+ 				centeredSlides: true,
+    			navigation: {
+    				nextEl: 'div.container > article > div.projects > div.description > div.button.next',
+    				prevEl: 'div.container > article > div.projects > div.description > div.button.prev'
+  					},
+  				on: {
+  					init: function () {
+    					$('div.container > article > div.projects > div.description > span').text($('div.container > article > div.projects > div.swiper-projects > ul > li.swiper-slide-active > div > span').text());
+    					},
+    				transitionStart: function () {
+    					$('div.container > article > div.projects > div.description > span').text($('div.container > article > div.projects > div.swiper-projects > ul > li.swiper-slide-active > div > span').text());
+    					}
+    				},
+    			pagination: {
+        			el: 'div.container > article > div.projects > div.swiper-projects > div.swiper-pagination',
+        			type: 'fraction',
+      				}
+				});
+			}, 50);
+    	}
+   	return false;
+	});
